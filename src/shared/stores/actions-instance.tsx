05ebac7f34d8ed55cced4c789a751af257eb0efc
AvilaAndre <avila_andre14@hotmail.com>
2024-05-05 13:01:48 +0100

    feat: differentiate between instance types

--- src/shared/stores/actions-instance.tsx
+++ src/shared/stores/actions-instance.tsx
@@ -2,9 +2,22 @@ import { invoke } from "@tauri-apps/api";
 import { createRoot, createSignal } from "solid-js";
 
 function createInstanceManagement() {
-	const [selectedInstance, setSelectedInstance] = createSignal({ empty: true });
+	const emptyInstance: {
+		empty: boolean,
+		name: string,
+		type: string,
+		data: unknown
+	} = {
+		empty: true,
+		name: "",
+		type: "",
+		data: undefined
+	}
+
+	const [selectedInstance, setSelectedInstance] = createSignal(emptyInstance);
 
 	const selectInstance = async (instance: string, config_name: string) => {
+		setSelectedInstance(emptyInstance);
 		await invoke("get_instances", { configName: config_name }).then(
 			(res: any) => {
 				for (let i = 0; i < res.length; i++) {
@@ -20,10 +33,10 @@ function createInstanceManagement() {
 				}
 			}
 		).catch(
-			(_) => { setSelectedInstance({ empty: true }); }
+			(_) => { setSelectedInstance(emptyInstance); }
 		);
 	}
-	const unselectInstance = () => setSelectedInstance({ empty: true });
+	const unselectInstance = () => setSelectedInstance(emptyInstance);
 
 	return { selectedInstance, selectInstance, unselectInstance };
 }
