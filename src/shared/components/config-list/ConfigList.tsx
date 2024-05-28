a61cc7df7c276e990e1f436cbe65f0d60e274709
AvilaAndre <avila_andre14@hotmail.com>
2024-05-28 01:15:03 +0100

    feat: show network on node apps and clients

--- src/shared/components/config-list/ConfigList.tsx
+++ src/shared/components/config-list/ConfigList.tsx
@@ -37,7 +37,9 @@ const ConfigList = () => {
 				}
 				} class="input input-bordered w-full" />
 				<div class="flex flex-row w-full gap-x-1">
-					<button class="btn btn-primary w-full shrink" onClick={() => createNewConfig(newConfigName())} disabled={newConfigName() == ""}>Create new config</button>
+					<button class="btn btn-primary w-full shrink" onClick={() => {
+						createNewConfig(newConfigName()); refreshConfigsList()
+					}} disabled={newConfigName() == ""}>Create new config</button>
 					<button class="btn btn-secondary btn-square shrink-0" onClick={refreshConfigsList}>
 						<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z" /></svg>
 					</button>
--- src/shared/components/instance/cards/instance-info.tsx
+++ src/shared/components/instance/cards/instance-info.tsx
@@ -1,10 +1,39 @@
-import { Match, Switch } from "solid-js";
+import { Match, Switch, createSignal } from "solid-js";
 import configManager, { InstanceData } from "../../../stores/config-manager";
+import verifyIP from "../../../utils/ip-verifier";
 
 export default function InstanceInfoCard(props: any) {
 	const instance: InstanceData = props.instance;
 
 	const { unselectInstance } = configManager;
+
+	const [networkAddress, setNetworkAddress] = createSignal("");
+	const [networkName, setNetworkName] = createSignal("");
+	const [subnet, setSubnet] = createSignal("");
+
+	const [existingNetworks, setExistingNetworks] = createSignal({});
+
+	const { addNewNodeAppInstance, setExistingNetworksMap } = configManager;
+
+	setExistingNetworksMap(setExistingNetworks);
+
+	async function addNewNode() {
+		if (
+			await addNewNodeAppInstance(
+				name(),
+				networkName(),
+				networkAddress(),
+				replicas()
+			)
+		) {
+			//reset
+			setName("");
+			setReplicas(1);
+			setNetworkName("");
+			setNetworkAddress("");
+		}
+	}
+
 
 	return (
 		<div class="card w-full bg-primary text-primary-content shadow-xl">
@@ -12,11 +41,19 @@ export default function InstanceInfoCard(props: any) {
 				<h2 class="card-title">{instance.name}</h2>
 				<h5> {instance.type} </h5>
 				<Switch fallback={<div />}>
-					<Match when={instance.type == "NodeApp"}>
-						NodeApp
-					</Match>
-					<Match when={instance.type == "Client"}>
-						Client
+					<Match when={instance.type == "Client" || instance.type == "NodeApp"}>
+						<label class="form-control w-full max-w-xs">
+							<div class="label">
+								<span class="label-text text-black">Network Address</span>
+							</div>
+							<input type="text" placeholder="" value={instance.data?.network_address ?? ""} class="input input-bordered bg-transparent w-full max-w-xs" />
+						</label>
+						<label class="form-control w-full max-w-xs">
+							<div class="label">
+								<span class="label-text text-black">Network Name</span>
+							</div>
+							<input type="text" placeholder="" value={instance.data?.network_name ?? ""} class="input input-bordered bg-transparent w-full max-w-xs" />
+						</label>
 					</Match>
 				</Switch>
 				<div class="card-actions justify-end">
